1291fbaee93bcc2c2a81e9354c8b93085dda7441
BeastCodZ <62057520+BeastCodZ@users.noreply.github.com>
2024-10-22 08:31:20 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,7 @@ const geistMono = localFont({
 export const metadata = {
   title: "BeastCodZ - Full Stack Developer",
   description: "Full-stack developer specializing in creating intelligent, scalable solutions that blend creativity with code.",
-  keywords: "BeastCodZ, Full Stack Developer, Web Development, JavaScript, React, Node.js, Next.js, Scalable Solutions, Creative Coding",
-  googleSiteVerification: "No1F5WTfQe4-jDTcQpVzZWVuhEfw7jvtDd1d7j1eEf0",
+  keywords: "BeastCodZ, Full Stack Developer, Web Development, JavaScript, React, Node.js, Next.js, Scalable Solutions, Creative Coding"
 };
 
 
@@ -28,6 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="No1F5WTfQe4-jDTcQpVzZWVuhEfw7jvtDd1d7j1eEf0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
