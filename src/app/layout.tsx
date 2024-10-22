799fc81cfb2022d0e4a4d49c49f713d3f7a7cf3f
BeastCodZ <62057520+BeastCodZ@users.noreply.github.com>
2024-10-22 08:26:13 +0530

    rawr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,13 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export const metadata: Metadata = {
-  title: "BeastCodZ",
-  description: "A Full Stack Developer",
+export const metadata = {
+  title: "BeastCodZ - Full Stack Developer",
+  description: "Full-stack developer specializing in creating intelligent, scalable solutions that blend creativity with code.",
+  keywords: "BeastCodZ, Full Stack Developer, Web Development, JavaScript, React, Node.js, Next.js, Scalable Solutions, Creative Coding",
+  googleSiteVerification: "No1F5WTfQe4-jDTcQpVzZWVuhEfw7jvtDd1d7j1eEf0",
 };
+
 
 export default function RootLayout({
   children,
